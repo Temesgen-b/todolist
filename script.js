a9b04c3489ae3d4cddfd6d8f9cacfd4963dee4e0
Temesgen <temesgenb439@gmail.com>
2024-11-27 02:54:33 -0800

    second commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const listcontainer =document.getElementById("list-container");
+const inputbox = document.getElementById("input-box"); 
+
+
+function addTask() {
+    if(inputbox.value === '') {
+        alert("You should write someting");
+    } else {
+        let li =document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer .appendChild(li);
+        let span =document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    } 
+    inputbox.value ="";
+    saveData();
+
+}
+
+
+listcontainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    } 
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);  
+
+/*fun save data*/
+
+function saveData(){
+    localStorage.setItem("data", listcontainer.innerHTML);
+}
+
+/*fun dont clear info when refiresh browser*/
+
+function sowTask(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}  
+sowTask();
